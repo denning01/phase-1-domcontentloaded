a4891b88122382b80ea3b641873006d2b035cb75
Denning <gichabadenning254@gmail.com>
2024-07-17 11:20:22 +0300

    changes text content

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', () => {
+    const textElement = document.getElementById('text');
+    textElement.textContent = 'This is really cool!';
+  });
